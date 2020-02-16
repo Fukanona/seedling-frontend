81755a261c5845097970376d5f1c2cfc59c2cad6
luizaes <luiza.engler@vtex.com.br>
2020-02-15 22:50:42 -0300

    Add login screen

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import '../App.css'
 import axios from 'axios'
+import seedImage from '../images/seed.png'
 
 function Login() {
 
  
 
   return ( 
-    <div align="center">
-      
-    </div>
+      <div>
+          <div style={{display: "flex", flexDirection: "row", justifyContent: "center"}}>
+                <h1> Seedling Online Judge</h1>
+                <img style={{marginTop: "17px"}} src={seedImage} alt="SOJ" height="42" width="42" />
+          </div>
+          <div style={{justifyContent:'center', alignItems: "center"}}>
+                <h3 style={{textAlign: "center"}}> Login</h3>
+                <form style={{textAlign: "center"}}>
+                    <p>
+                        <label style={{textAlign: "center"}}>
+                            Username:
+                            <input type="text" name="username" />
+                        </label>
+                    </p>
+                    <p>
+                        <label style={{textAlign: "center"}}>
+                            Password:
+                            <input type="password" name="password" />
+                        </label>
+                    </p>
+                    <input type="submit" value="Enviar" />
+                </form>
+          </div>
+      </div>
   )
 }
 
